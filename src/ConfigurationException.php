093f641490a4673208770d198125335b77b0fa19
Marcin Pudełek <mrcnpdlk@gmail.com>
2019-03-30 20:20:40 +0100

    first commit

---
+++ src/ConfigurationException.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by Marcin.
+ * Date: 30.03.2019
+ * Time: 20:06
+ */
+
+namespace Mrcnpdlk\Lib;
+
+/**
+ * Class ConfigurationException
+ *
+ * @package Mrcnpdlk\Lib
+ */
+class ConfigurationException extends Exception
+{
+
+}
---
+++ src/ConfigurationOptionsAbstract.php
@@ -0,0 +1,70 @@
+<?php
+/**
+ * Created by Marcin.
+ * Date: 30.03.2019
+ * Time: 20:05
+ */
+
+namespace Mrcnpdlk\Lib;
+
+use Psr\Log\LoggerInterface;
+
+/**
+ * Class ConfigurationOptionsAbstract
+ *
+ * @package Mrcnpdlk\Lib
+ */
+abstract class ConfigurationOptionsAbstract
+{
+    /** @noinspection PhpUndefinedClassInspection */
+    /**
+     * @var \Psr\Log\LoggerInterface
+     */
+    protected $logger;
+
+    /**
+     * ConfigurationOptions constructor.
+     *
+     * @param array $config
+     *
+     * @throws \Mrcnpdlk\Lib\ConfigurationException
+     */
+    public function __construct(array $config = [])
+    {
+        /* @noinspection PhpUndefinedClassInspection */
+        $this->logger = new NullLogger();
+
+        foreach ($config as $key => $value) {
+            $funName = sprintf('set%s', ucfirst($key));
+            if (method_exists($this, $funName)) {
+                $this->$funName($value);
+            } elseif (property_exists($this, $key)) {
+                $this->{$key} = $value;
+            } else {
+                throw new ConfigurationException(sprintf('Property "%s" not defined in Config class "%s"', $key, __CLASS__));
+            }
+        }
+    }
+
+    /** @noinspection PhpUndefinedClassInspection */
+
+    /**
+     * @return \Psr\Log\LoggerInterface
+     */
+    public function getLogger(): LoggerInterface
+    {
+        return $this->logger;
+    }
+
+    /**
+     * @param \Psr\Log\LoggerInterface $logger
+     *
+     * @return $this
+     */
+    public function setLogger(LoggerInterface $logger): self
+    {
+        $this->logger = $logger;
+
+        return $this;
+    }
+}
